970598199e77b533690f8849860110586dadba6a
akanation4 <103402576+akanation4@users.noreply.github.com>
2022-12-08 09:45:51 +0900

    ソースに直接書き込む形式のベースを作成

--- UnityProject/Assets/GameManager.cs
+++ UnityProject/Assets/GameManager.cs
@@ -5,94 +5,76 @@ using UnityEngine.UI;
 using System.IO;
 using UnityEditor;
 
-class StoryBlock {
-    public string story;
-    public string option1text;
-    public string option2text;
-    public StoryBlock option1Block;
-    public StoryBlock option2Block;
-
-    public StoryBlock(string story, string option1text = "", string option2text = "",
-        StoryBlock option1Block = null, StoryBlock option2Block = null) {
-
-        this.story = story;
-        this.option1text = option1text;
-        this.option2text = option2text;
-        this.option1Block = option1Block;
-        this.option2Block = option2Block;
-    }
-}
-
-
-
-
-
-public class GameManager : MonoBehaviour {
+/// <summary>
+/// ゲームの管理を行うクラス
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Unity上で設定する
     public Text mainText;
-    public Button option1;
-    public Button option2;
     public InputField inputField;
+    public Image image;
 
-    StoryBlock currentBlock;
+    public int count = 0;
 
-    static StoryBlock block8 = new StoryBlock("You decided to sit here forever. Game over", "", "");
-    static StoryBlock block7 = new StoryBlock("You decided to go back to the start. Game over", "", "");
-    static StoryBlock block6 = new StoryBlock("You decided to go back to the start. Game over", "", "", block7, block8);
-    static StoryBlock block5 = new StoryBlock("You decided to go back to the start. Game over", "Inspect floor", "", block6, block2);
-    static StoryBlock block4 = new StoryBlock("You decided to go back to the start. Game over", "Check your pockets", "Inspect floor", block5, block6);
-    static StoryBlock block3 = new StoryBlock("You noticed a big, wooden doors on the opposite side of the room. After quick inspection, it looks like they are closed. But there is a key lock!", "Inspect floor", "Do nothing", block1, block8);
-    static StoryBlock block2 = new StoryBlock("You started to panic and screamed for help, but it looks like, you're here completry alone.", "Check the doors", "Sit down", block3, block8);
-    static StoryBlock block1 = new StoryBlock("You just woke up in a small, dark cell in an old castel.", "Look for other people", "Check the doors", block2, block3);
-
-
-
-    // Start is called before the first frame update
+    /// <summary>
+    /// 初期化
+    /// </summary>
     void Start()
     {
-        // DisplayBlock(block1);
-
-        // string path = Application.dataPath + "/story.csv";
-        // bool isAppend = true;
-
-        // using (StreamReader fs = new StreamReader(path))
-        // {
-        //     while (fs.Peek() != -1)
-        //     {
-        //         Debug.Log(fs.ReadLine());
-        //     }
-        // }
-
+        // コンポーネントを取得
         inputField = inputField.GetComponent<InputField>();
         mainText = mainText.GetComponent<Text>();
+        image = image.GetComponent<Image>();
 
-        mainText.text = "Enter 'Hello, world'";
-        inputField.text = "";
+        /* 000 */
+        inputField.enabled = false;
+        DisplayText("Hello, world");
+        DisplayImage("start");
     }
 
-    void DisplayBlock(StoryBlock block) {
-        Debug.Log(currentBlock);
-        mainText.text = block.story;
-        option1.GetComponentInChildren<Text>().text = block.option1text;
-        option2.GetComponentInChildren<Text>().text = block.option2text;
-
-        currentBlock = block;
-        Debug.Log(currentBlock.option1Block);
-    }
-
-    public void Button1Clicked() {
-        Debug.Log("Button 1 clicked");
-        DisplayBlock(currentBlock.option1Block);
-    }
-
-    public void Button2Clicked() {
-        DisplayBlock(block2);
-    }
-
-    public void InputText() {
-        if (inputField.text == "Hello, world") {
-            mainText.text = "Good! Hello, world";
-        } else {
-            mainText.text = "Wrong! Try again";
+    /// <summary>
+    /// 毎フレーム呼び出される
+    /// </summary>
+    void Update()
+    {
+        // エンターキーが押されたらストーリーを進行する
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            count++;
+            switch (count)
+            {
+                /* 001 */
+                case 1:
+                    inputField.enabled = true;
+                    DisplayText("What is your name?");
+                    break;
+                /* 002 */
+                case 2:
+                    inputField.enabled = false;
+                    DisplayText("Hello, " + inputField.text);
+                    break;
+                default:
+                    break;
+            }
         }
+    }
+
+    /// <summary>
+    /// テキストを表示する
+    /// </summary>
+    /// <param name="text">表示するテキスト</param>
+    public void DisplayText(string text)
+    {
+        mainText.text = text;
+    }
+
+    /// <summary>
+    /// 画像を表示する
+    /// </summary>
+    /// <param name="path">画像のパス(ファイル名・拡張子なし)</param>
+    public void DisplayImage(string path)
+    {
+        image.sprite = Resources.Load<Sprite>("Pic/" + path);
     }
 }
